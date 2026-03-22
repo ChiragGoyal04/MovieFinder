69946e7e6dafad3058be34515560594cf3ea0bea
Chirag Goyal <chiraggoyal90455@gmail.com>
2026-03-22 18:54:36 +0530

    New Project

---
+++ scripts.js
@@ -0,0 +1,33 @@
+async function getMovie() {
+
+    let yourKey="5326bb0c";
+
+    let movieName=document.getElementById("search-bar").value;
+
+    let url=`http://www.omdbapi.com/?t=${movieName}&apikey=${yourKey}`;
+
+    let response= await fetch(url);
+
+    let data= await response.json();
+
+    if(data.Response=="False"){
+        alert("Movie not found/Invalid Name/Still not added in our database");
+        return;
+    }
+    console.log(data);
+    if(data.Poster!=="NA"){
+    document.getElementById('movie_image').style.backgroundImage=`url('${data.Poster}')`;
+    }
+
+
+    document.getElementById('title-content').innerText=data.Title;
+    document.getElementById('year-content').innerText=data.Year;
+    document.getElementById('runtime-content').innerText=data.Runtime;
+    document.getElementById('genre-content').innerText=data.Genre;
+    document.getElementById('director-content').innerText=data.Director;
+    document.getElementById('imdb-content').innerText=data.Ratings[0].Value;
+    document.getElementById('plot-content').innerText=data.Plot;
+
+    document.getElementById('movie-image').innerText=data.Title;
+    
+}
